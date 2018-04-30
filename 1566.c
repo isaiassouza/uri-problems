55457bfa6dfe47976dcbf6df424f2c28ea84487a
isaiassouza <38844871+isaiassouza@users.noreply.github.com>
2018-04-30 12:37:38 -0300

    Update 1566.c

--- 1566.c
+++ 1566.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    long long int y,j,i,nc,n;
+    long long int x,y,j,i,nc,n;
     scanf("%lld",&nc);
     for(i=0; i<nc; i++)
     {
@@ -10,18 +10,18 @@ int main()
         for(j=0; j<n; j++)
             scanf("%lld",&v[j]);
 
-        for(y=1; y<n; y++)
-            for(j=0; j<n-1; i++)
+        for(y=1; y<=n; y++)
+            for(j=0; j<n-1; j++)
                 if(v[j]>v[j+1])
                 {
-                    long long int x =v[j];
+                    x =v[j];
                     v[j]=v[j+1];
                     v[j+1]=x;
                 }
 
-        for(j=0; j<n; j++)
-            printf("%lld ",v[i]);
-        printf("%lld\n",v[i]);
+        for(j=0; j<n-1; j++)
+            printf("%lld ",v[j]);
+        printf("%lld\n",v[j]);
     }
     return 0;
 }
